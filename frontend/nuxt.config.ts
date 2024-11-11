03a80296092bd178b0226fe5397eaf959d63f72a
kinjotatsuro <kinjo.tatsuro@rue.co.jp>
2024-11-11 15:56:23 +0900

    修正

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  plugins: ['~/plugins/axios']
+  plugins: ['~/plugins/axios'],
+  modules: [
+  ],
 })
