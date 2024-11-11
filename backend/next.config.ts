1c432d1152ca0105bb252b360c02c33085d896e3
kinjotatsuro <kinjo.tatsuro@rue.co.jp>
2024-11-11 16:49:31 +0900

    nextconfig修正

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true, // ここにtrailingSlashの設定を追加
 };
 
 export default nextConfig;
