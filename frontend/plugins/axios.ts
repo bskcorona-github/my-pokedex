f0cf52745282e25d2738fdc23d5289e6e46cd0db
kinjotatsuro <kinjo.tatsuro@rue.co.jp>
2024-11-11 18:20:53 +0900

    endpoint

--- frontend/plugins/axios.ts
+++ frontend/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default defineNuxtPlugin(() => {
   const instance = axios.create({
-    baseURL: 'https://my-pokedex-28uz.vercel.app/api',// Next.jsのAPIエンドポイント
+    baseURL: 'https://my-pokedex-backend.vercel.app/api',// Next.jsのAPIエンドポイント
   })
   return {
     provide: {
