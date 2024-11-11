5d8d7a34f9cfddaf847bd2b07f7998594343ac9e
kinjotatsuro <kinjo.tatsuro@rue.co.jp>
2024-11-11 16:35:12 +0900

    aiueo

--- frontend/plugins/axios.ts
+++ frontend/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default defineNuxtPlugin(() => {
   const instance = axios.create({
-    baseURL: 'http://localhost:3001/api', // Next.jsのAPIエンドポイント
+    baseURL: 'https://your-backend-project.vercel.app/api',// Next.jsのAPIエンドポイント
   })
   return {
     provide: {
